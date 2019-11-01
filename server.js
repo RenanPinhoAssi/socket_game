e51e1cec63f3dde8d9d22ea0624729b88f006841
Renan Pinho Assi <renanpinhoassi@gmail.com>
2019-11-01 18:29:54 -0300

    Changes

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 // Dependencies
+var SHOW_MODE = false;
+var PORT = (SHOW_MODE ? 5000 : 5010);
+
 var express = require("express");
 var http = require("http");
 var path = require("path");
@@ -6,7 +9,7 @@ var socketIO = require("socket.io");
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
-app.set("port", 5000);
+app.set("port", PORT);
 app.use("/static", express.static(__dirname + "/static"));
 
 // Routing
@@ -15,15 +18,25 @@ app.get("/", function(request, response) {
 });
 
 // Starts the server.
-server.listen(5000, function() {
-	console.log("Starting server on port 5000");
+server.listen(PORT, function() {
+	console.log("Starting server on port " + PORT);
 });
+
+var PLAYER_SETTINGS = require('./player/player.json');
+var MAP_SETTINGS    = require('./maps/regular.json');
+var TURF_SETTINGS  = require('./maps/turfs.json');
+
+
 
 var PLAYERS = {};
 var BULLETS = [];
-var SPEED = 0.5;
-var BULLET_SPEED = 0.7;
-var COLORS = ["green","red","blue","pink","cyan","black","gray","purple","brown"];
+
+var BULLET_PLAYER_SPEED = 2;
+var BULLET_DISTANCE = 800;
+
+var PLAYER_SPEED = PLAYER_SETTINGS["SPEED"];
+var PLAYER_SKIN  = PLAYER_SETTINGS["SKIN"];
+var PLAYER_SIZE  = PLAYER_SETTINGS["SIZE"];
 
 io.on("connection", function(socket) {
 	socket.on("disconnect", function() {
@@ -32,40 +45,58 @@ io.on("connection", function(socket) {
 
 	socket.on("new player", function() {
         PLAYERS[socket.id] = {};        
-        PLAYERS[socket.id]["coordinates"] = {x: 300,y: 300};
-        PLAYERS[socket.id]["color"] = COLORS[Math.floor(Math.random()*COLORS.length)];
+        PLAYERS[socket.id]["coordinates"] = {
+            x: 0,//Math.random() * MAP_SETTINGS["width"],
+            y: 0//Math.random() * MAP_SETTINGS["height"]
+        };
+        PLAYERS[socket.id]["color"] = PLAYER_SKIN[Math.floor(Math.random()*PLAYER_SKIN.length)];
         PLAYERS[socket.id]["data"] = {
             up: false,
             down: false,
             left: false,
             right: false
         };
-        
+        PLAYERS[socket.id]["alive"] = true;
+        PLAYERS[socket.id]["key"] = socket.id;
+        socket.emit('startgame', 
+            {
+                "MAP": MAP_SETTINGS,
+                "TURF": TURF_SETTINGS,
+                "PLAYER": {
+                    "GLOBAL": PLAYER_SETTINGS,
+                    "PERSONAL": {
+                        "key": socket.id,
+                        "color": PLAYERS[socket.id]["color"],
+                        "xi": PLAYERS[socket.id]["coordinates"].x,
+                        "yi": PLAYERS[socket.id]["coordinates"].y,
+                    }
+                }
+            }
+        );      
     });
     
     socket.on("movement", function(data) {
         try{
-            PLAYERS[socket.id]["data"] = data;
+            if(PLAYERS[socket.id]["alive"]){
+                PLAYERS[socket.id]["data"] = data;
+            }
         }catch(e){}
     });
 
-    socket.on("changes", function(data) {
-        socket.broadcast.emit('update', data);   
-    });
-
     socket.on("shoot", function(data) {
-        let xi = PLAYERS[socket.id]["coordinates"].x;
-        let yi = PLAYERS[socket.id]["coordinates"].y;
-        let xDelta = data.x - xi;
-        let yDelta = data.y - yi;
-        let angle = yDelta/xDelta;
-        let bullet = 
-        {
-            "x": xi,
-            "y": yi,
-            "angle": angle
+        if(PLAYERS[socket.id]["alive"]){
+            let angle = data;
+            let bullet = 
+            {
+                "x": PLAYERS[socket.id]["coordinates"].x,
+                "y": PLAYERS[socket.id]["coordinates"].y,
+                "angle": angle,
+                "shoot_date": new Date().getTime(),
+                "distance_time": BULLET_DISTANCE,
+                "owner": PLAYERS[socket.id]
+            }
+            BULLETS.push(bullet);
         }
-        BULLETS.push(bullet);
     });
 });
 
@@ -78,26 +109,50 @@ setInterval(function() {
         player = PLAYERS[key];
         let data = player["data"];
         if (data.left) {
-            player["coordinates"].x -= SPEED*timeDifference;
+            player["coordinates"].x -= PLAYER_SPEED*timeDifference;
         }
         if (data.up) {
-            player["coordinates"].y -= SPEED*timeDifference;
+            player["coordinates"].y -= PLAYER_SPEED*timeDifference;
         }
         if (data.right) {
-            player["coordinates"].x += SPEED*timeDifference;
+            player["coordinates"].x += PLAYER_SPEED*timeDifference;
         }
         if (data.down) {
-            player["coordinates"].y += SPEED*timeDifference;
+            player["coordinates"].y += PLAYER_SPEED*timeDifference;
         }
         lastUpdateTime = currentTime;
     }
 
     for(var i in BULLETS){
-        BULLETS[i]["x"] = BULLETS[i]["x"] + BULLET_SPEED*timeDifference*Math.cos(BULLETS[i]["angle"]);
-        BULLETS[i]["y"] = BULLETS[i]["y"] + BULLET_SPEED*timeDifference*Math.sin(BULLETS[i]["angle"]);
+        if(currentTime - BULLETS[i]["shoot_date"] > BULLETS[i]["distance_time"]){
+            BULLETS.splice(i,1);
+        }else{
+            let line_components = {};
+            line_components.xi = BULLETS[i]["x"];
+            line_components.yi = BULLETS[i]["y"];
+            BULLETS[i]["x"] += ( BULLET_PLAYER_SPEED*Math.cos( BULLETS[i]["angle"] ) )*timeDifference;
+            BULLETS[i]["y"] += ( BULLET_PLAYER_SPEED*Math.sin( BULLETS[i]["angle"] ) )*timeDifference;
+            line_components.xf = BULLETS[i]["x"];
+            line_components.yf = BULLETS[i]["y"];
+
+            for(var key in PLAYERS){
+                if(PLAYERS[key] != BULLETS[i]["owner"]){
+                    player = PLAYERS[key];
+                    let p_xc = player["coordinates"].x + (PLAYER_SIZE/2);
+                    let p_yc = player["coordinates"].y + (PLAYER_SIZE/2);
+                    let distance = (p_xc - line_components.xf)*(p_xc - line_components.xf) + (p_yc - line_components.yf)*(p_yc - line_components.yf)
+                    distance = Math.sqrt(distance);
+                    if(distance < PLAYER_SIZE*1.5){
+                        PLAYERS[key]["alive"] = false;
+                    }
+                }
+            }
+
+            
+        }
         lastUpdateTime = currentTime;
     }
     
-    io.sockets.emit('state', {"players": PLAYERS, "bullets": BULLETS});       
+    io.sockets.emit('renderUpdate', {"players": PLAYERS, "bullets": BULLETS});       
 }, 1000 / 60);
 
--- static/game.js
+++ static/game.js
@@ -1,5 +1,13 @@
 var socket = io();
 
+var viewport;
+var map_layer;
+var player_layer;
+
+var viewport_context;
+var map_layer_context;
+var player_layer_context;
+
 var movement = {
 	up: false,
 	down: false,
@@ -7,81 +15,181 @@ var movement = {
 	right: false
 };
 var change = false;
-document.addEventListener("keydown", function(event) {
-	switch (event.keyCode) {
-		case 65: // A
-            change = !movement.left; 
-            movement.left = true;
-			break;
-		case 87: // W
-            change = !movement.up; 
-			movement.up = true;
-			break;
-		case 68: // D
-            change = !movement.right; 
-			movement.right = true;
-			break;
-		case 83: // S
-            change = !movement.down; 
-			movement.down = true;
-			break;
-	}
-	var dom_obj = document.getElementById("test").value;
-	socket.emit("changes", dom_obj);  
-    if(change){
-        socket.emit("movement", movement);  
-        change = false;
-    }
-});
-document.addEventListener("keyup", function(event) {
-	switch (event.keyCode) {
-		case 65: // A
-			movement.left = false;
-			break;
-		case 87: // W
-			movement.up = false;
-			break;
-		case 68: // D
-			movement.right = false;
-			break;
-		case 83: // S
-			movement.down = false;
-			break;
-    }
-    socket.emit("movement", movement);  
-});
+var shooting = false;
+var aim_direction;
 
-document.getElementById("canvas").addEventListener("click", function(event){
-	let aim_direction = {x:event.clientX, y:event.clientY};
-	socket.emit("shoot", aim_direction);  
-});
+var MAP_SETTINGS;
+var TURF_SETTINGS;
+var PLAYER_SETTINGS;
 
-socket.emit("new player");
-var canvas = document.getElementById("canvas");
-canvas.width = 800;
-canvas.height = 600;
-var context = canvas.getContext("2d");
+var VIEWPORT_HEIGHT;
+var VIEWPORT_WIDTH;
+var VIEWPORT_CENTER_X;
+var VIEWPORT_CENTER_Y;
+var PLAYER_SIZE;
 
-socket.on("state", function(data) {
-	context.clearRect(0, 0, 800, 600);
-	for (var id in data["players"]) {
-		console.log(data["players"][id])
-		var player = data["players"][id]["coordinates"];
-        context.fillStyle = data["players"][id]["color"];
-		context.beginPath();
-		context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-		context.fill();
-	}
+var MAP_HEIGHT;
+var MAP_WIDTH;
+
+var initialize_player = function() {
+	player_layer_context.fillStyle = PLAYER_INFO["color"];
+	player_layer_context.beginPath();
+	player_layer_context.arc(
+		VIEWPORT_CENTER_X,
+		VIEWPORT_CENTER_Y,
+		PLAYER_SIZE,
+		0,
+		2*Math.PI
+	);
+	player_layer_context.fill();
+};
+
+var initialize_map = function() {
+	let map_data = MAP_SETTINGS["tile-map"];
+	let map_size = MAP_SETTINGS["tile-size"];
+	let x;
+	let y = 0;
+	for (var j = 0; j < map_size; j++) {
+		for (var i = 0; i < map_size; i++) {
+			x = map_size * i;
+			let this_turf = TURF_SETTINGS[map_data[i + 100 * j]];
+			map_layer_context.fillStyle = this_turf["color"];
+			map_layer_context.beginPath();
+			map_layer_context.fillRect(x, y, map_size, map_size);
+		}
+		y += map_size;
+	};
+};
+
+var initialize_game = function(data) {
+	VIEWPORT_HEIGHT = $(window).height();
+	VIEWPORT_WIDTH = $(window).width();
+
+	MAP_SETTINGS = data["MAP"];
+	TURF_SETTINGS = data["TURF"];
+	PLAYER_SETTINGS = data["PLAYER"]["GLOBAL"];
+	PLAYER_INFO = data["PLAYER"]["PERSONAL"];
 	
-	for (var i in data["bullets"]) {
-		let bullet = data["bullets"][i];
-		context.fillStyle = "black";
-		context.beginPath();
-		context.arc(bullet.x, bullet.y, 3, 0, 2 * Math.PI);
-		context.fill();
-	}
-});
+	PLAYER_SIZE = PLAYER_SETTINGS["SIZE"];
 
-socket.on("update", function(data) {
-	document.getElementById("test").value = data;
-})
+	MAP_HEIGHT = MAP_SETTINGS["height"];
+	MAP_WIDTH = MAP_SETTINGS["width"];
+	
+	VIEWPORT_CENTER_X = (VIEWPORT_WIDTH / 2) - (PLAYER_SIZE/2);
+	VIEWPORT_CENTER_Y = (VIEWPORT_HEIGHT / 2) - (PLAYER_SIZE/2);
+
+	viewport = document.getElementById("viewport");
+	map_layer = document.getElementById("map_layer");
+	player_layer = document.getElementById("player_layer");
+
+	viewport_context = viewport.getContext("2d");
+	map_layer_context = map_layer.getContext("2d");
+	player_layer_context = player_layer.getContext("2d");
+	
+	viewport.width = VIEWPORT_WIDTH;
+	viewport.height = VIEWPORT_HEIGHT;
+
+	player_layer.width = VIEWPORT_WIDTH;
+	player_layer.height = VIEWPORT_HEIGHT;
+
+	map_layer.width = MAP_WIDTH;
+	map_layer.height = MAP_HEIGHT;
+
+
+	viewport_context.translate(VIEWPORT_WIDTH/2 - PLAYER_SIZE/2,VIEWPORT_HEIGHT/2- PLAYER_SIZE/2);
+	$("#map_layer").css("left",-(PLAYER_INFO["xi"]-VIEWPORT_WIDTH/2+ PLAYER_SIZE/2));
+	$("#map_layer").css("top",-(PLAYER_INFO["yi"]-VIEWPORT_HEIGHT/2+ PLAYER_SIZE/2));
+
+	initialize_map();
+	initialize_player();
+
+	document.addEventListener("keydown", function(event) {
+		switch (event.keyCode) {
+			case 65: // A
+				change = !movement.left;
+				movement.left = true;
+				break;
+			case 87: // W
+				change = !movement.up;
+				movement.up = true;
+				break;
+			case 68: // D
+				change = !movement.right;
+				movement.right = true;
+				break;
+			case 83: // S
+				change = !movement.down;
+				movement.down = true;
+				break;
+		}
+
+		if (change) {
+			socket.emit("movement", movement);
+			change = false;
+		}
+	});
+	document.addEventListener("keyup", function(event) {
+		switch (event.keyCode) {
+			case 65: // A
+				movement.left = false;
+				break;
+			case 87: // W
+				movement.up = false;
+				break;
+			case 68: // D
+				movement.right = false;
+				break;
+			case 83: // S
+				movement.down = false;
+				break;
+		}
+		socket.emit("movement", movement);
+	});
+	document.getElementById("player_layer").addEventListener("mouseup", function(event) {
+		let dx = event.clientX - VIEWPORT_CENTER_X;
+		let dy = event.clientY - VIEWPORT_CENTER_Y;
+		let angle = Math.atan2(dy,dx);
+		socket.emit("shoot", angle);
+	});
+};
+
+$(document).ready(function() {
+	socket.emit("new player");
+	socket.on("startgame", function(data) {
+		initialize_game(data);
+		let test = $("#map_layer");
+		socket.on("renderUpdate", function(data) {
+			viewport_context.clearRect(-VIEWPORT_WIDTH/2,-VIEWPORT_HEIGHT/2,2*VIEWPORT_WIDTH,2*VIEWPORT_HEIGHT);
+			
+			let players_set = data["players"];
+			
+			$("#map_layer").css("left",-(players_set[PLAYER_INFO["key"]]["coordinates"].x-VIEWPORT_WIDTH/2+ PLAYER_SIZE/2));
+			$("#map_layer").css("top",-(players_set[PLAYER_INFO["key"]]["coordinates"].y-VIEWPORT_HEIGHT/2+ PLAYER_SIZE/2));
+			// map_layer_context.translate(players_set[PLAYER_INFO["key"]].x,players_set[PLAYER_INFO["key"]].y);
+			// let myself = players_set[PLAYER_INFO["key"]]["coordinates"];
+			delete players_set[PLAYER_INFO["key"]];
+			for (var id in players_set) {
+				if (players_set[id]["alive"]) {
+					var player = players_set[id]["coordinates"];
+					viewport_context.fillStyle = players_set[id]["color"];
+					viewport_context.beginPath();
+					viewport_context.arc(
+						player.x,
+						player.y,
+						PLAYER_SIZE,
+						0,
+						2 * Math.PI
+					);
+					viewport_context.fill();
+				}
+			}
+			for (var i in data["bullets"]) {
+				let bullet = data["bullets"][i];
+				viewport_context.fillStyle = "black";
+				viewport_context.beginPath();
+				viewport_context.arc(bullet.x, bullet.y, 3, 0, 2 * Math.PI);
+				viewport_context.fill();
+			}
+		});
+	});
+});
